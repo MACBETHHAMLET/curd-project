10c6fcfff88e2b56900ef5352f7b43a974c3268e
Maryam Mirzaee <maryammirzaieu@yahoo.com>
2023-05-13 17:53:05 +0330

    +better error handling +using product.toString() for printing the products +delete command +update command

--- cli/src/main/java/org/project/Main.java
+++ cli/src/main/java/org/project/Main.java
@@ -28,6 +28,19 @@ public class Main implements Runnable {
         System.out.println("Welcome to Farmers Market Admin Panel\navailable commands are: \n  create,list,update,delete\nuse these commands to add,view,update and delete products on your shop.");
     }
 
+    public static void printError(Response r) {
+        switch (r.code()) {
+            case 404:
+                System.out.println("404: product not found");
+                break;
+            case 500:
+                System.out.println("500: internal server error");
+                break;
+            default:
+                System.out.println("unrecognised error code: " + r.code());
+        }
+    }
+
     @CommandLine.Command(name = "create", mixinStandardHelpOptions = true, description = "adds a new product to webshop")
     static
     class CreateCommand implements Runnable {
@@ -49,7 +62,6 @@ public class Main implements Runnable {
 
         @Override
         public void run() {
-
             try {
                 Product newProduct = new Product(0, name, weight, inStock, price, img);
                 RequestBody body = RequestBody.create(json.writeValueAsString(newProduct), JSON);
@@ -58,14 +70,12 @@ public class Main implements Runnable {
                 if (response.isSuccessful()) {
                     if (response.body() != null) {
                         Product createdProduct = json.readValue(response.body().string(), Product.class);
-                        System.out.printf("Product was successfully added to database\n#%d %s  %s $  %s Kg  %d in stock  image file:%s\n%n",
-                                createdProduct.getId(), createdProduct.getName(), createdProduct.getPrice(), createdProduct.getWeight(), createdProduct.getInStock(), createdProduct.getImg());
+                        System.out.printf("%s\nAbove product was successfully added to database.", createdProduct);
                     } else {
                         System.out.println("No product was created");
                     }
-                } else {
-                    System.out.printf("Error %d : %s%n", response.code(), response.message());
-                }
+                } else  printError(response);
+
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -88,17 +98,13 @@ public class Main implements Runnable {
                         List<Product> products = json.readValue(response.body().string(), new TypeReference<>() {
                         });
 
-                        String formattedOutput = products.subList(0, Math.min(limit, products.size())).stream().map(
-                                        p -> String.format("#%d %s  %s $  %s Kg  %d in stock  image file:%s\n",
-                                                p.getId(), p.getName(), p.getPrice(), p.getWeight(), p.getInStock(), p.getImg()))
+                        String formattedOutput = products.subList(0, Math.min(limit, products.size())).stream().map( p -> String.format("%s\n", p))
                                 .collect(Collectors.joining());
                         System.out.println(formattedOutput);
                     } else {
                         System.out.println("No products were found");
                     }
-                } else {
-                    System.out.printf("Error %d : %s%n", response.code(), response.message());
-                }
+                }  else  printError(response);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -114,7 +120,14 @@ public class Main implements Runnable {
 
         @Override
         public void run() {
-
+            Call call = client.newCall(new Request.Builder().url(baseURL + "/" + id).delete().build());
+            try (Response response = call.execute()) {
+                if (response.isSuccessful()) {
+                    System.out.printf("Product #%d was successfully deleted from database.", id);
+                }else  printError(response);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -129,13 +142,13 @@ public class Main implements Runnable {
         String name;
 
         @CommandLine.Option(names = {"-p", "--price"}, description = "Product price (USD).")
-        float price;
+        float price = -1;
 
         @CommandLine.Option(names = {"-s", "--stock"}, description = "Number in Stock.")
-        int inStock;
+        int inStock = -1;
 
         @CommandLine.Option(names = {"-w", "--weight"}, description = "Product weight (Kg).")
-        float weight;
+        float weight = -1;
 
         @CommandLine.Option(names = {"-i", "--image"}, description = "Product image file path (relative to static/ folder)")
         String img;
@@ -143,7 +156,38 @@ public class Main implements Runnable {
 
         @Override
         public void run() {
+            try {
+                Response getResponse = client.newCall(new Request.Builder().url(baseURL + "/" + id).build()).execute();
+                if (getResponse.isSuccessful()) {
+                    if (getResponse.body() != null) {
+                        Product product = json.readValue(getResponse.body().string(), Product.class);
+                        Product updatedProduct = new Product(product.getId(),
+                                name != null ? name : product.getName(),
+                                weight != -1 ? weight : product.getWeight(),
+                                inStock != -1 ? inStock : product.getInStock(),
+                                price != -1 ? price : product.getPrice(),
+                                img != null ? img : product.getImg()
+                        );
+                        RequestBody body = RequestBody.create(json.writeValueAsString(updatedProduct), JSON);
+                        Call call = client.newCall(new Request.Builder().url(baseURL).post(body).build());
+                        Response response = call.execute();
 
+                        if (response.isSuccessful()) {
+                            if (response.body() != null) {
+                                Product updatedProductInDB = json.readValue(response.body().string(), Product.class);
+                                System.out.printf("%s\nchanged to:\n%s", product,updatedProductInDB);
+                            } else {
+                                System.out.println("No product was updated");
+                            }
+                        }else  printError(response);
+
+                    } else {
+                        System.out.println("No products were found");
+                    }
+                } else  printError(getResponse);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
