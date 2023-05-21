00259196e84c94026bf2fdab44ed804b76cee477
Maryam Mirzaee <maryammirzaieu@yahoo.com>
2023-05-21 11:41:25 +0330

    added cart items and their total to models of MainPage.html

--- webshop/src/main/java/com/example/webshop/controller/MainController.java
+++ webshop/src/main/java/com/example/webshop/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.webshop.controller;
 
+import com.example.webshop.repo.CartRepo;
 import com.example.webshop.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,15 @@ public class MainController {
     @Autowired
     private ProductRepo productRepo;
 
+    @Autowired
+    private CartRepo cartRepo;
+
     @GetMapping("/")
     public String mainPage(Model model) {
         model.addAttribute("products", productRepo.findAll());
+        model.addAttribute("cart", cartRepo.findAll());
+        float total = cartRepo.findAll().stream().map(c -> c.getQuantity() * c.getProduct().getPrice()).reduce(0f, Float::sum);
+        model.addAttribute("total", String.format("%.2f", total) );
         return "MainPage.html";
     }
 }
